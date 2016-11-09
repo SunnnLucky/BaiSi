470772999acee779cae53df6d5416e82d041d35b
sunlei <455319106@qq.com>
2016-11-09 19:23:30 +0800

    初始化项目

---
+++ BaiSi/BaiSi/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  BaiSi
+//
+//  Created by 孙磊 on 2016/11/9.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ BaiSi/BaiSi/ViewController.m
@@ -0,0 +1,29 @@
+//
+//  ViewController.m
+//  BaiSi
+//
+//  Created by 孙磊 on 2016/11/9.
+//  Copyright © 2016年 Sun. All rights reserved.
+//
+
+#import "ViewController.h"
+
+@interface ViewController ()
+
+@end
+
+@implementation ViewController
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view, typically from a nib.
+}
+
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+
+@end
